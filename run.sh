4885af260cec4fc249053dbba3eee2a8029fdb6d
Alexander Walters <tritium@sdamon.com>
2014-09-12 02:29:10 -0400

    removed status check after cleanup

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ if [ $? -ne 0 ]; then exit $?; fi
 ./Tcf.exe
 if [ $? -ne 0 ]; then exit $?; fi
 rm Tcf.exe
-if [ $? -ne 0 ]; then exit $?; fi
 echo; echo;
 
 echo "Java"
@@ -19,7 +18,6 @@ if [ $? -ne 0 ]; then exit $?; fi
 java Tcf
 if [ $? -ne 0 ]; then exit $?; fi
 rm Tcf.class
-if [ $? -ne 0 ]; then exit $?; fi
 echo; echo;
 
 echo "Python"
