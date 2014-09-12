e5b4a44c07632d791b08e5a9f5277f30ca703c40
Alexander Walters <tritium@sdamon.com>
2014-09-12 07:51:52 -0400

    Made run.py more sane, i think

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from collections import namedtuple
 from subprocess import check_output, check_call, CalledProcessError
 from os import unlink
 from ConfigParser import SafeConfigParser
+from sys import argv, exit
 
-Job = namedtuple('Job', 'name version source build run cleanup')
+Job = namedtuple('Job', 'name versions source build run cleanup')
 
 
 def _sec(instr):
@@ -15,38 +16,84 @@ def _sub(instr):
     return '\033[31m' + instr + '\033[0m'
 
 
-def run(jobs, show_source=True):
+def print_ver(job):
+    try:
+        versions = [check_output(v, shell=True) for v in job.versions]
+    except:
+        raise
+    else:
+        print _sub('Version')
+        print '\n'.join(versions)
+
+
+def print_source(job):
+    sources = []
+    for sf in job.source:
+        try:
+            with open(sf) as f:
+                sources.append((sf, f.read()))
+        except OSError:
+            pass
+    if sources:
+        print _sub('Source Code')
+        for name, text in sources:
+            print _sub(name)
+            print text
+
+
+def build(job):
+    for step in job.build:
+        check_call(step, shell=True)
+
+
+def run(job):
+    try:
+        out = [check_output(step, shell=True) for step in job.run]
+    except:
+        raise
+    else:
+        print _sub('Standard Out')
+        print '\n'.join(out)
+
+
+def cleanup(job):
+    for f in job.cleanup:
+        try:
+            unlink(f)
+        except OSError:
+            pass
+
+
+def execute(jobs, show_source=True):
     for job in jobs:
         print _sec('Language: ' + job.name)
         try:
-            print _sub('Version')
-            for ver in job.version:
-                print check_output(ver, shell=True)
+            print_ver(job)
             if show_source:
-                print _sub('Source Code')
-                for sf in job.source:
-                    with open(sf) as f:
-                        print _sub('File: ' + sf)
-                        print f.read()
-            for step in job.build:
-                check_call(step, shell=True)
-            print _sub('Standard Out')
-            for step in job.run:
-                print check_output(step, shell=True)
+                print_source(job)
+            build(job)
+            run(job)
         except CalledProcessError as e:
-            print e
+            return e
         finally:
-            for f in job.cleanup:
-                try:
-                    unlink(f)
-                except OSError:
-                    pass
+            cleanup(job)
+
+
+def get_jobs(conf='run.conf'):
+    config = SafeConfigParser()
+    config.read(conf)
+    langs = config.sections()
+    jobs = [
+        Job._make([l] + [v.split('\n') for k, v in config.items(l)])
+        for l in langs
+    ]
+    return jobs
+
+
+def main(argv):
+    jobs = get_jobs()
+    return execute(jobs)
 
 
 if __name__ == '__main__':
-    config = SafeConfigParser()
-    config.read('run.conf')
-    langs = config.sections()
-    jobs = [[l] + [v.split('\n') for k, v in config.items(l)] for l in langs]
-    jobs = map(Job._make, jobs)
-    run(jobs)
+    exit(main(argv))
