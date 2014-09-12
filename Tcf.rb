100f719dc670aafcc565a2ca4cf273555ec58eac
Alexander Walters <tritium@sdamon.com>
2014-09-12 11:31:26 -0400

    Added ruby else and retry

--- Tcf.rb
+++ Tcf.rb
@@ -1,14 +1,22 @@
+first = true
 begin
-    begin
-        raise "Foo"
-    rescue => detail
-        puts "Inner Catch"
-        raise detail
-    ensure
-        puts "Inner Finally"
+    if first
+        begin
+            raise "Foo"
+        rescue => detail
+            puts "Inner Catch"
+            raise detail
+        ensure
+            puts "Inner Finally"
+        end
     end
 rescue
     puts "Outer Catch"
+    first = false
+    puts "Retry fires the begin block again"
+    retry
+else
+    puts "Else runs if there is no exceptions"
 ensure
     puts "Outer Finally"
 end
